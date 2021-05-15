d0080502d6319b1f44478bf61ea01c52c5ac210a
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-05-15 15:23:36 +0300

    Generate required components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { NewQuoteFormComponent } from './new-quote-form/new-quote-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    NewQuoteFormComponent
   ],
   imports: [
     BrowserModule,
