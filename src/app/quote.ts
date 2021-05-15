e6c047d213237279c8b048b8eb85825ff2d3cc3b
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-05-15 21:37:53 +0300

    Hide form.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,5 @@
 export class Quote {
     constructor(public id:number, public quote:string, public author:string, public submitter:string, public dateSubmitted:Date, public upvotes:number, public downvotes:number){
-        
+
     }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -15,7 +15,8 @@ export class QuoteComponent implements OnInit {
     quotes: Quote[] = [
         new Quote(1, "I think it\'s fair to say that personal computers have become the most empowering tool we\'ve ever created. They\'re tools of communication, they\'re tools of creativity, and they can be shaped by their user.", "Bill Gates", "Peter Kennedy", new Date(2020,8,29),29, 12),
         new Quote(2, "Whether you want to uncover the secrets of the universe, or you just want to pursue a career in the 21st century, basic computer programming is an essential skill to learn.", "Stephen Hawking", "Peter Kennedy", new Date(2018,5,23),54, 23),
-        new Quote(3, "I taught myself how to program computers when I was a kid, bought my first computer when I was 10, and sold my first commercial program when I was 12.", "Elon Musk", "Peter Kennedy", new Date(2020,4,1), 76, 6)
+        new Quote(3, "I taught myself how to program computers when I was a kid, bought my first computer when I was 10, and sold my first commercial program when I was 12.", "Elon Musk", "Peter Kennedy", new Date(2020,4,1), 76, 6),
+        new Quote(4, "If future generations are to remember us more with gratitude than sorrow, we must achieve more than just the miracles of technology. We must also leave them a glimpse of the world as it was created, not just as it looked when we got through with it.", "Lyndon B. Johnson", "Peter Kennedyb", new Date(2021,5,12), 20, 13)
     ];
 
     deleteQuote(index){
@@ -23,6 +24,10 @@ export class QuoteComponent implements OnInit {
             if(toDelete){
                 this.quotes.splice(index, 1);
             }
+    }
+    showForm:boolean = false;
+    toggleForm(){
+        this.showForm = !this.showForm;
     }
   constructor() { }
 
